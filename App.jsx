b01f9317fe414c4c315656327c902b3e52f94eaf
Swayam Shivam <143308636+HazardKrish@users.noreply.github.com>
2025-10-11 00:27:40 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Pricing from './pages/Pricing';
+import FAQ from './pages/FAQ';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/faq" element={<FAQ />} />
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default App;
